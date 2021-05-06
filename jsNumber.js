2ac5a8c079f5be13fca028d36abc4101ff31814c
Sakib12345 <sakibalislam12345@gmail.com>
2021-05-06 07:09:07 +0600

    done

---
+++ jsNumber.js
@@ -0,0 +1,11 @@
+//js number
+const mathMark = 90;
+console.log(isNaN(mathMark)) //false
+console.log(!isNaN(mathMark)) //true
+
+//parseFloat(),parseInt()
+const banglaMark = 80.56;
+const englishMark = 67;
+console.log(parseInt(banglaMark)); //convert to integer
+console.log(parseFloat(englishMark));//convert to float
+
